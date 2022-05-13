3d3964ec76d5d21567dfbea6eadf7c97b91fe85e
Iftekhar Md. Shishir <84434133+iftekharweb@users.noreply.github.com>
2022-05-14 01:18:31 +0600

    Update Basic_Information.cpp

--- Basic_Information.cpp
+++ Basic_Information.cpp
@@ -1,4 +1,10 @@
 
-# Predicate Function
-  https://www.youtube.com/watch?v=JAfJssvFgDI
-  https://www.spoj.com/problems/EKO/
+##    Monotonic Predicate Function
+
+SOURCE:
+      https://www.youtube.com/watch?v=JAfJssvFgDI
+PROBLEMS:
+      https://www.spoj.com/problems/EKO/
+      https://codeforces.com/problemset/problem/165/B
+      https://codeforces.com/problemset/problem/812/C
+      https://codeforces.com/problemset/problem/230/B
